61e2819dc1fd24280f842420bac7f4ed742b2e6e
Safiyah <safiyahmasud@gmail.com>
2023-08-18 15:37:47 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Slider from './components/Slider'
 import Body from './components/Body'
+import Members from './components/Members'
 import DemandOptions from './components/DemandOptions'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 			<Hero />
 			<Slider />
 			<Body />
+      <Members />
 			<DemandOptions />
 		</>
 	)
