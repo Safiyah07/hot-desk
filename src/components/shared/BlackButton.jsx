c1968988576422fd3b815273fce588e53ea3635b
Safiyah <safiyahmasud@gmail.com>
2023-08-18 15:20:45 +0100

    .

--- src/components/shared/BlackButton.jsx
+++ src/components/shared/BlackButton.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 export default function BlackButton({ children }) {
 	return (
 		<div className='bg-black font-bold w-fit text-sm cursor-pointer text-white p-6 uppercase hover:bg-transparent hover:text-black border-2 border-black transition ease-in-out'>
-			<h2>{children}</h2>
+			{children}
 		</div>
 	)
 }
