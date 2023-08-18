285f7e208b926210195f5284c145064bab417547
Safiyah <safiyahmasud@gmail.com>
2023-08-18 01:50:39 +0100

    body contd

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,71 +6,73 @@ import BodyImage3 from '../assets/bodyImage3.jpg'
 export default function Body() {
 	return (
 		<>
-			<div className='flex uppercase'>
+			<div className='flex md:flex-col sm:flex-col uppercase'>
 				<div className='md:w-full sm:w-full lg:w-1/2'>
-					<div className='text-black px-12 lg:py-10 md:py-16 sm:py-16'>
+					<div className='text-black px-12 md:px-9 sm:px-5 lg:py-10 md:py-10 sm:py-10'>
 						<div>
-							<h2 className='lg:text-[21px] md:text-[40px] lg:pb-32 sm:text-[21px] uppercase text-black font-extrabold'>
+							<h2 className='text-[21px] md:text-[16px] sm:text-[16px] lg:pb-32 uppercase text-black font-extrabold'>
 								Hot Desk Your Way
 							</h2>
 						</div>
 
-						<div className='md:text-2xl sm:text-xl normal-case font-bold pt-8 md:text-center sm:text-center'>
-							<h2 className='lg:text-[54px] lg:text-5xl lg:pb-32 md:text-center sm:text-center'>
+						<div className='md:text-2xl sm:text-xl normal-case font-bold pt-8 '>
+							<h2 className='lg:text-[54px] lg:text-5xl lg:pb-32 '>
 								SILENT OR BUZZING. SPACES FOR EVERY VIBE.
 							</h2>
 							<br />
-							<p className='lg:text-2xl md:text-2xl sm:text-xl normal-case lg:pb-20 '>
+							<p className='lg:text-2xl md:text-xl sm:text-lg normal-case lg:pb-20 md:pb-10 sm:pb-10'>
 								At Hotdesk, we have spaces for everyone to do their best work –
 								whether that’s amongst buzzing activity or tucked away in a cozy
 								nook.
 							</p>
-							<h2 className='flex md:justify-center sm:justify-center'>
+							<h2 className='flex'>
 								<Button>See Memberships</Button>
 							</h2>
 							<br />
 						</div>
 					</div>
 				</div>
-				<div className='w-1/2 flex justify-center m-auto py-48 sm:hidden bg-gradient-to-bl from-[#9492f0] via-white to-[#92f0ce] '>
+				<div className='lg:w-1/2 flex justify-center lg:py-48 md:py-10 sm:py-10 bg-gradient-to-bl from-[#9492f0] via-white to-[#92f0ce] '>
 					<img
 						src={BodyImage1}
 						alt=''
-						className='w-[70%] h-[70%] rounded-full border-2 border-black'
+						className=' md:w-[50%] md:[h-50%] sm:[w-45%] sm:[h-45%] rounded-full border-4 border-black'
 					/>
 				</div>
 			</div>
 
-			<div className='flex uppercase'>
-				<div className='lg:w-1/2 lg:flex gap-4 justify-center items-center h-fit pt-[12rem] pb-[15rem] sm:hidden bg-black'>
+			<div className='flex md:flex-col-reverse sm:flex-col-reverse uppercase'>
+				<div className='lg:w-1/2 flex gap-4 justify-center items-center h-fit lg:pt-[12rem] lg:pb-[15rem] md:py-[7rem] sm:py-[4rem] bg-black overflow-hidden'>
 					<img
 						src={BodyImage2}
 						alt=''
-						className='w-[70%] h-[70%] border-2 border-white'
+						className='w-[70%] h-[70%] md:w-[55%] md:[h-55%] sm:[w-45%] sm:[h-45%] border-4 border-white'
 					/>
-          <img
+					<img
 						src={BodyImage3}
 						alt=''
-						className='w-[70%] h-[70%] rounded-full border-2 border-white'
+						className='w-[70%] h-[70%] md:w-[55%] md:h-[55%] sm:[w-50%] sm:[h-50%] rounded-full border-4 border-white'
 					/>
 				</div>
 				<div className='md:w-full sm:w-full lg:w-1/2 z-10'>
-					<div className='text-black bg-white px-12 lg:py-10 md:py-16 sm:py-16'>
+					<div className='text-black bg-white px-12 md:px-9 sm:px-5 lg:py-10 md:py-10 sm:py-10'>
 						<div>
-							<h2 className='lg:text-[21px] md:text-[40px] lg:pb-32 sm:text-[21px] uppercase text-black font-extrabold'>
-              NOT JUST DESKS
+							<h2 className='lg:text-[21px] md:text-[16px] lg:pb-32 sm:text-[16px]  uppercase text-black font-extrabold'>
+								NOT JUST DESKS
 							</h2>
 						</div>
 
-						<div className='md:text-2xl sm:text-xl normal-case font-bold pt-8 md:text-center sm:text-center'>
-							<h2 className='lg:text-[54px] lg:text-5xl lg:pb-32 md:text-center sm:text-center'>
-              CREATIVE SPACES FOR RECORDING, PERFORMANCE AND MORE.
+						<div className='md:text-2xl sm:text-xl normal-case font-bold pt-8 '>
+							<h2 className='lg:text-[54px] lg:text-5xl lg:pb-32 '>
+								CREATIVE SPACES FOR RECORDING, PERFORMANCE AND MORE.
 							</h2>
 							<br />
-							<p className='lg:text-2xl md:text-2xl sm:text-xl normal-case lg:pb-20 '>
-              At Hotdesk, we have spaces for everyone to do their best work – whether that’s amongst buzzing activity or tucked away in a cozy nook.
+							<p className='lg:text-2xl md:text-xl sm:text-lg normal-case lg:pb-20 md:pb-10 sm:pb-10 '>
+								At Hotdesk, we have spaces for everyone to do their best work –
+								whether that’s amongst buzzing activity or tucked away in a cozy
+								nook.
 							</p>
-							<h2 className='flex md:justify-center sm:justify-center'>
+							<h2 className='flex'>
 								<Button>Explore Facilities</Button>
 							</h2>
 							<br />
