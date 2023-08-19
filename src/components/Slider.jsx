74ae465937783a108fb00448b9d42c17a17ad6fe
Safiyah <safiyahmasud@gmail.com>
2023-08-19 15:43:40 +0100

    .

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,23 +1,50 @@
-import { useScroll, useTransform } from 'framer-motion'
+// import { useScroll, useTransform } from 'framer-motion'
+import { useRef, useEffect } from 'react'
+import gsap from 'gsap'
 
 export default function Slider() {
-	const { scrollYProgress } = useScroll()
-	const x = useTransform(scrollYProgress, [0, 1], [0, 60])
+	const marqueeRef = useRef(null)
+
+	useEffect(() => {
+		const marqueeContent = marqueeRef.current
+		const marqueeContainer = marqueeContent.parentNode
+
+		const contentWidth = marqueeContent.offsetWidth
+		const containerWidth = marqueeContainer.offsetWidth
+
+		const offset = 220 // Adjust this value as needed
+		const animationDuration = (contentWidth + offset) / 90 // Adjust as needed
+
+		gsap.set(marqueeContent, { x: containerWidth + offset }) // Initial position
+
+		const animation = gsap.to(marqueeContent, {
+			x: -contentWidth,
+			duration: animationDuration,
+			repeat: -1, // Infinite loop
+			ease: 'none',
+		})
+
+		return () => {
+			animation.kill() // Clean up animation on unmount
+		}
+	}, [])
 
 	return (
 		<>
-			<div className='bg-black w-full lg:py-40 md:py-16 sm:py-12 lg:text-5xl md:text-3xl sm:text-xl text-white'>
-				<marquee scrollamount='10'>
-					<div className='flex gap-10 uppercase  tracking-wider pb-16 font-extrabold overflow-hidden scroll-text sm:text-center'>
-						<span>Hawthorn</span>
-						<span>Richmond</span>
-						<span>South Yarra</span>
-						<span className='flex items-center gap-3'>
-							Winsor <h2 className='badge badge-white p-5 text-xl'>new!</h2>
-						</span>
-					</div>
-				</marquee>
-				<p className='flex justify-center font-bold text-center px-4'>
+			<div className='w-full text-white bg-black lg:py-40 md:py-16 sm:py-12 lg:text-5xl md:text-3xl sm:text-xl'>
+				<div
+					ref={marqueeRef}
+					className='flex gap-10 pb-16 overflow-hidden font-extrabold tracking-wider uppercase scroll-text sm:text-center'
+				>
+					<span>Hawthorn</span>
+					<span>Richmond</span>
+					<span>SouthYarra</span>
+					<span className='flex items-center gap-3'>
+						Winsor <h2 className='p-5 text-xl badge badge-white'>new!</h2>
+					</span>
+				</div>
+
+				<p className='flex justify-center px-4 font-bold text-center'>
 					New creative co-working spaces opening in 2023.
 				</p>
 			</div>
