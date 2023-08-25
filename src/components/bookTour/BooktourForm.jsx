476857869979ed249ea1abeb7e5587fdd93ecb12
Safiyah <safiyahmasud@gmail.com>
2023-08-25 16:44:27 +0100

    Book-tour page

---
+++ src/components/bookTour/BooktourForm.jsx
@@ -0,0 +1,80 @@
+import BlackButton from "../shared/BlackButton"
+
+export default function BooktourForm() {
+  return (
+    <div>
+      <>
+			<div className='text-black bg-gradient-to-br from-[#c7f092] via-white to-[#9492f0] py-24'>
+				<div className='bg-arrow sm:bg-none bg-no-repeat lg:bg-[top_right_15rem] bg-[top_right_6rem] flex flex-col  text-center'>
+					<h2 className='lg:text-[44px] md:text-[2.3rem] sm:text-2xl font-black uppercase lg:w-1/2 m-auto sm:w-full md:px-40 sm:px-12 sm:pb-2'>
+						HOW CAN HOTDESK WORK FOR YOUR TEAM?
+					</h2>
+					<p className='pb-20 text-3xl font-extrabold md:text-xl sm:text-lg sm:pb-10 sm:px-3'>
+						Hit the form and we’ll be in touch to discuss.
+					</p>
+
+					<form className='flex flex-col items-center justify-center m-auto sm:w-[90%]'>
+						<div className='flex justify-center w-full lg:gap-20 md:flex-col sm:flex-col'>
+							<div className='flex flex-col lg:w-1/2'>
+								<label className='mb-4 text-sm font-black text-left uppercase'>
+									Your Name
+								</label>
+								<input
+									type='text'
+									name='Name'
+									id='Name'
+									className='h-16 mb-4 border'
+								/>
+							</div>
+
+							<div className='flex flex-col lg:w-1/2'>
+								<label className='mb-4 text-sm font-black text-left uppercase'>
+									Email Address
+								</label>
+								<input
+									type='email'
+									name='email'
+									id='email'
+									className='h-16 mb-4 border'
+								/>
+							</div>
+						</div>
+
+						<div className='flex justify-center gap-20'>
+							<div className='flex flex-col md:w-[90vw] sm:w-[87vw] lg:w-[65.7vw]'>
+								<label className='mb-4 text-sm font-black text-left uppercase'>
+									Company name (Optional)
+								</label>
+								<input
+									type='text'
+									name='company-name'
+									id='company-name'
+									className='h-16 mb-4 border'
+								/>
+
+								<label className='mb-4 text-sm font-black text-left uppercase'>
+									Comments or questions
+								</label>
+								<textarea
+									name=''
+									id=''
+									maxLength={5000}
+									cols='30'
+									rows='10'
+									className='mb-4 border'
+								></textarea>
+
+								<div className='my-10'>
+									<BlackButton>submit form</BlackButton>
+								</div>
+
+								<p className='text-lg font-extrabold'>See you soon! ✌</p>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</>
+    </div>
+  )
+}
--- src/components/contact/Hero.jsx
+++ src/components/contact/Hero.jsx
@@ -5,10 +5,10 @@ export default function Hero() {
 	return (
 		<section className='relative font-black text-black'>
 			<div className='flex gap-32 md:gap-20 sm:gap-12 sm:flex-col md:flex-col'>
-				<div className='flex flex-col w-full gap-10 py-20 pl-20 md:py-8 sm:pl-5'>
+				<div className='flex flex-col w-full gap-10 py-20 pl-20 md:py-8 sm:py-6 sm:pl-5'>
 					<div className='sticky top-40'>
 						<div className='flex flex-col gap-6'>
-							<h2 className='lg:text-[60px] md:text-[40px] sm:text-[25px] uppercase'>
+							<h2 className='lg:text-[60px] md:text-[40px] sm:text-[20px] uppercase'>
 								Lets Chat
 							</h2>
 							<p className='text-[24px] md:text-[18px] sm:text-[18px]'>
@@ -42,7 +42,7 @@ export default function Hero() {
 							</div>
 						</div>
 
-						<div className='flex flex-col gap-10 py-10 md:mx-12   border-b-4 border-[#c7f092]'>
+						<div className='flex flex-col gap-10 py-10 md:mx-12   border-b-4 border-[#9492f0]'>
 							<h2 className='uppercase lg:text-5xl md:text-3xl sm:text-xl'>Richmond</h2>
 							<div className='flex flex-col gap-5'>
 								<div className='lg:text-[20px] text-[18px]'>
@@ -58,7 +58,7 @@ export default function Hero() {
 							</div>
 						</div>
 
-						<div className='flex flex-col gap-10 py-10 md:mx-12   border-b-4 border-[#c7f092]'>
+						<div className='flex flex-col gap-10 py-10 md:mx-12   border-b-4 border-[#92f0ce]'>
 							<h2 className='uppercase lg:text-5xl md:text-3xl sm:text-xl'>SOUTH YARRA</h2>
 							<div className='flex flex-col gap-5'>
 								<div className='lg:text-[20px] text-[18px]'>
