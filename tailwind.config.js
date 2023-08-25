0df622760babd693a028b366008ffeaea30d8071
Safiyah <safiyahmasud@gmail.com>
2023-08-25 19:27:27 +0100

    vercel reload

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,9 @@ export default {
       }
 		},
 		screens: {
-      // 'sm': '576px',
-      // => @media (min-width: 576px) { ... }
-      
 			sm: { max: '519px' },
-			// => @media (min-width: 640px) { ... }
-
 			md: {min:'520px', max: '992px'},
 			lg: {min:'993px'},
-			// => @media (min-width: 1024px) { ... }
 		},
 	},
 	// eslint-disable-next-line no-undef
